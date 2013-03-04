23f40e56d0b865d9230ddd75533640f1515b77c9
Chris <chris@Linux-VirtualBox.(none)>
2013-03-04 00:38:04 -0800

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SentimentAnalysis::Application.config.secret_token = '0f4d284e7861a6d0943587087149517becf2b9d1420cf5593d6381286feb648f5f2fe108a56098a5100e96b96401be26c5425faa49592869eae7ed0a7acff230'
